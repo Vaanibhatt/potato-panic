de8187f36df9502a1c68b7a2b6cd2e0ef46ffac1
Vaanibhatt <vaanibhatt956@gmail.com>
2025-12-25 02:20:28 -0800

    Fixed Flickering Object  and cleaned code

--- src/Game.java
+++ src/Game.java
@@ -1,42 +1,31 @@
 package src;
-import javax.swing.JFrame;
+
 
 public class Game {
     public static void main(String[] args) {
-        JFrame jframe = new JFrame("Potato Panic");
-
         GameCanvas gameCanvas = new GameCanvas();
-        gameCanvas.setIgnoreRepaint(true);
-
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jframe.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        jframe.setIgnoreRepaint(true);
-        jframe.add(gameCanvas);
-        jframe.pack();
-        jframe.setLocationRelativeTo(null);
-        jframe.setVisible(true);
         run(gameCanvas);
 
     }
-//Fix this later
+
     public static void run(GameCanvas gameCanvas) {
 
         final double fps = 60.0;
-        final double  secondsPerFrame =  1_000_000_000.0 / fps;
+        final double secondsPerFrame = 1_000_000_000.0 / fps;
         double now = System.nanoTime();
-        double lastFrame =  System.nanoTime();
+        double lastFrame = System.nanoTime();
         double delta = 0;
 
         while (true) {
-            
+
             now = System.nanoTime();
             delta = now - lastFrame;
-            if (delta > secondsPerFrame)
-            {
+            if (delta > secondsPerFrame) {
                 gameCanvas.render();
                 lastFrame = now;
             }
-            
+
         }
     }
+
 }
--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -1,42 +1,56 @@
 package src;
 
-
 import java.awt.*;
 import java.awt.image.BufferStrategy;
 
-public class GameCanvas extends Canvas  {
+import javax.swing.JFrame;
 
+public class GameCanvas extends Canvas {
     private BufferStrategy bufferStrategy = null;
 
     private int x = 0;
     private int y = 0;
     private int speed = 5;
 
+    public GameCanvas() {
+        JFrame jframe = new JFrame("Potato Panic");
+
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        jframe.setLocationRelativeTo(null);
+
+        jframe.add(this);
+
+        this.setIgnoreRepaint(true);
+        jframe.setVisible(true);
+
+    }
+
     public void render() {
 
-        if (bufferStrategy == null)
-        {
+        if (bufferStrategy == null) {
             createBufferStrategy(1);
             bufferStrategy = getBufferStrategy();
         }
 
+       Graphics graphics = bufferStrategy.getDrawGraphics();
         do {
-            do {
-                Graphics2D graphics = (Graphics2D) bufferStrategy.getDrawGraphics();
-                graphics.clearRect(0, 0, 2000, 2000);
 
-                // Draw circle
-                graphics.setColor(Color.RED);
-                graphics.fillOval(x, y, 50, 50);
+            graphics.setColor(new Color(255, 255, 255));
+            graphics.fillRect(0, 0, 2000, 2000);
 
-                graphics.dispose();
-                x += speed;
-                y += speed;
-            } while (bufferStrategy.contentsRestored());
+            // Draw circle
+            graphics.setColor(Color.RED);
+            graphics.fillOval(x, y, 50, 50);
+
+            // Remove this test later
+            x += speed;
+            y += speed;
 
             bufferStrategy.show();
-            
-        } while (bufferStrategy.contentsLost());
-    }
 
+        } while (bufferStrategy.contentsRestored() || bufferStrategy.contentsLost());
+        graphics.dispose();
+        
+    }
 }
